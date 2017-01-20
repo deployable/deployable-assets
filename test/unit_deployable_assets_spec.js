b8aa12fc6851ae69c31ec7b6a87764d533e1f4b9
Matt Hoyle <matt@deployable.co>
2017-01-20 05:36:14 +0000

    fix font and add tests for all helpers

--- test/unit_deployable_assets_spec.js
+++ test/unit_deployable_assets_spec.js
@@ -6,8 +6,31 @@ describe('unit::deployable-assets::DeployableAssets', function(){
 
   describe('class', function(){
 
+    let ass = null
+
+    beforeEach(function(){
+      ass = new DeployableAssets({prefix: 'https://cdn.site/assets'})
+    })
+
     it('should create an instance', function(){
-      expect( new DeployableAssets() ).to.be.ok
+      expect( ass ).to.be.ok
+    })
+
+    it('should create a js script', function(){
+      expect( ass.js('js/some.js') ).to.equal( '<script src="https://cdn.site/assets/js/some.js" type="application/javascript"></script>' )
+    })
+
+    it('should create a css link tag', function(){
+      expect( ass.css('css/some.css') ).to.equal( '<link rel="stylesheet" type="text/css" href="https://cdn.site/assets/css/some.css"/>' )
+    })
+
+    it('should create font css', function(){
+      expect( ass.font('Wingdings', 'font/wd.woff') ).to.match( /^@font-face \{[\s\S]+\}$/ )
+    })
+
+    it('should create font css with url', function(){
+      let re = new RegExp('https://cdn.site/assets/font/arial.woff')
+      expect( ass.font('Arial', 'font/arial.woff') ).to.match( re )
     })
 
   })
